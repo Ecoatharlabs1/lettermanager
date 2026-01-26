8ab1493b6d0cac52624fd8fc60936803ec9ffec9
Ecoatharlabs <ecoatharlabs@gmail.com>
2026-01-27 00:03:10 +0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "20dea4cd81372ca4e66bd8338030f0da",
+"flutter_bootstrap.js": "6132cfe7b3513c18aedc12b445529294",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "5436238672d7796da155bca90931417d",
 "/": "5436238672d7796da155bca90931417d",
-"main.dart.js": "88f1133a6d6f38038e1c2b718e5c7447",
+"main.dart.js": "e9d9d3e4ebeccb9020636f0e842429e9",
 "manifest.json": "089d4d12380665619bb8935b130e3780",
 "version.json": "95f9466860bf64db0e261927a63c09e5"};
 // The application shell files that are downloaded before a service worker can
